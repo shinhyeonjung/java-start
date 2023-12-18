a1f63793560d32e0e50e15cbd532448f4fe87023
shinhyeonjung <forstudyit23@gmail.com>
2023-12-18 23:54:35 +0900

    [Feat] 연산자

---
+++ src/operator/Operator1.java
@@ -0,0 +1,30 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+
+        // 뺄셈
+        int diff = a - b;
+        System.out.println("a - b = " + diff);
+
+        // 곱셈
+        int multi = a * b;
+        System.out.println("a * b = " + multi);
+
+        // 나눗셈
+        int div = a / b;
+        System.out.println("a / b = " + div); // 2가 출력됨 => 같은 int형끼리 계산하면 결과도 int형
+
+        // 나머지
+        int mod = a % b;
+        System.out.println("a % b = " + mod);
+
+    }
+}
