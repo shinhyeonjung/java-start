e2ca240f71e5476f5d43b6f09abf447c732a4f2c
shinhyeonjung <forstudyit23@gmail.com>
2023-12-28 03:02:41 +0900

    [Feat] 반복문

---
+++ src/loop/ex/WhileEx3.java
@@ -0,0 +1,15 @@
+package loop.ex;
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+        int max = 100;
+        int i = 1;
+        int sum = 0;
+
+        while(i <= max){
+            sum = sum + i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
